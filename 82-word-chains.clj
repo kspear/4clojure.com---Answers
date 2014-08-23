8ec06c07eb3f3a10550cc9c35532add18b844fa2
kspear <kendallsutton@gmail.com>
2014-08-22 23:16:50 -0400

    82 - Tidied.

--- 82-word-chains.clj
+++ 82-word-chains.clj
@@ -1,39 +1,25 @@
-(fn word-chain
-  ([words]
-   (if (first
-        (remove
-         nil?
-         (flatten
-          (for [w words]
-            (word-chain (disj words w) [w])))))
-     true false))
-  ([words chain]
-   (if (not-empty words)
-     (let [l (last chain)]
-       (for [w words]
-         (if ((fn [w1 w2]
-                (let [[w1 w2] (sort-by count > [w1 w2])]
-                  (cond
-                   (= (count w1) (count w2))
-                   (some #(not (nil? %))
-                         ((fn match-sequential-letters [[w1 w2]]
-                            (for [i (range (count w1))]
-                              (let [s (str (apply str (take i w1))
-                                           #"[\w]"
-                                           (apply str (drop (inc i) w1)))
-                                    pattern (re-pattern s)]
-                                (when (re-find pattern w2) (str pattern)))))
-                          [w1 w2]))
+(fn [w]
+  (letfn [(word-chain
+           ([words]
+            (let [words (for [w words]
+                          (word-chain (disj words w) [w]))]
+              (if (some true? (flatten words)) true false)))
+           ([words chain]
+            (if (not-empty words)
+              (let [l (last chain)]
+                (for [w words]
+                  (if (matcher [l w])
+                    (word-chain (disj words w) (conj chain w)))))
+              true)))
 
-                   (= (dec (count w1)) (count w2))
-                   ((fn match-sequential-letters [[w1 w2]]
-                      (for [i (range (count w1))]
-                        (let [s (str (apply str (take i w1))
-                                     #"[\w]"
-                                     (apply str (drop (inc i) w1)))
-                              pattern (re-pattern s)]
-                          (when (re-find pattern w2) (str pattern)))))
-                    [w1 w2]))))
-              l w)
-           (word-chain (disj words w) (conj chain w)))))
-     chain)))
+          (matcher
+           [words]
+           (let [[w1 w2] (sort-by count < words)
+                 min-matches (dec (apply max (map count words)))
+                 matches (for [l w1]
+                           (re-seq (re-pattern (str l)) w2))]
+             (= min-matches
+                (->> matches
+                     (remove nil?)
+                     count))))]
+    (word-chain w)))
